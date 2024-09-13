d87d4efbfa90865dc46dc0d2d57fcc347314c45e
kky-ykk <kishanyadavcs21@gmail.com>
2024-09-14 01:43:08 +0530

    commit02

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
-import chair from "./assets/chair.svg";
+import l1 from "./assets/api-tableLamp2.svg";
+import l2 from "./assets/api-tableLamp.svg";
 
 function App() {
   return (
     <>
       <h1>Google Drive Image Example</h1>
       <img 
-        src={chair} 
+        src={l1}
+        alt="Google Drive Image" 
+        style={{ width: '300px', height: '300px' }} 
+      />
+      <img 
+        src={l2}
         alt="Google Drive Image" 
         style={{ width: '300px', height: '300px' }} 
       />
